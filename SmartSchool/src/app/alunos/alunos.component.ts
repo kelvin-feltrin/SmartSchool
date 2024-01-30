bdff68b0a8e611c36053cc4d74bee48102b9cda2
kelvin-feltrin <kelvin-798@hotmail.com>
2024-01-29 21:18:37 -0300

    Tabela Bootstrap

--- SmartSchool/src/app/alunos/alunos.component.ts
+++ SmartSchool/src/app/alunos/alunos.component.ts
@@ -10,13 +10,13 @@ export class AlunosComponent implements OnInit {
   titulo = 'Alunos';
 
   alunos = [
-    { nome: 'Marta' },
-    { nome: 'Paula' },
-    { nome: 'Laura' },
-    { nome: 'Luiza' },
-    { nome: 'Lucas' },
-    { nome: 'Pedro' },
-    { nome: 'Paulo' }
+    { id: 1, nome: 'Marta', sobrenome: 'Kent', telefone: 332255 },
+    { id: 2, nome: 'Paula', sobrenome: 'Isabela', telefone: 332255 },
+    { id: 3, nome: 'Laura', sobrenome: 'Antonia', telefone: 332255 },
+    { id: 4, nome: 'Luiza', sobrenome: 'Maria', telefone: 332255 },
+    { id: 5, nome: 'Lucas', sobrenome: 'Machado', telefone: 332255 },
+    { id: 6, nome: 'Pedro', sobrenome: 'Alvares', telefone: 332255 },
+    { id: 7, nome: 'Paulo', sobrenome: 'José', telefone: 332255 }
   ];
   constructor() { }
 
--- SmartSchool/src/app/professores/professores.component.ts
+++ SmartSchool/src/app/professores/professores.component.ts
@@ -9,12 +9,12 @@ export class ProfessoresComponent implements OnInit {
 
   public titulo = 'Professores';
 
-  professores = [
-    { nome: 'Lauro' },
-    { nome: 'Roberto' },
-    { nome: 'Ronaldo' },
-    { nome: 'Rodrigo' },
-    { nome: 'Alexandre' }
+  public professores = [
+    { id: 1, nome: 'Lauro', disciplina: 'Matemática' },
+    { id: 2, nome: 'Roberto', disciplina: 'Física' },
+    { id: 3, nome: 'Ronaldo', disciplina: 'Português' },
+    { id: 4, nome: 'Rodrigo', disciplina: 'Inglês' },
+    { id: 5, nome: 'Alexandre', disciplina: 'Programação' }
   ]
 
   constructor() { }
