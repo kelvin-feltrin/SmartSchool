91ff43fa2ce0a3bcaeb9d98779231bc043ff0945
kelvin-feltrin <kelvin-798@hotmail.com>
2024-02-13 13:50:02 -0300

    Criando controllers

---
+++ SmartSchool-WebAPI/Controllers/AlunoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSchool_WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlunoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Marcos");
+        }
+    }
+}
---
+++ SmartSchool-WebAPI/Controllers/ProfessorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSchool_WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfessorController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Vinicius");
+        }
+    }
+}
