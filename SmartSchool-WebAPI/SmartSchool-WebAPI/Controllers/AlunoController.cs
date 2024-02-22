32395eae2cf96015187ee9fdb747897c040aeea1
kelvin-feltrin <kelvin-798@hotmail.com>
2024-02-21 22:13:03 -0300

    Desenvolvendo GetAluno

--- SmartSchool-WebAPI/SmartSchool-WebAPI/Controllers/AlunoController.cs
+++ SmartSchool-WebAPI/SmartSchool-WebAPI/Controllers/AlunoController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartSchool_WebAPI.Data;
 using System;
+using System.Threading.Tasks;
 
 namespace SmartSchool_WebAPI.Controllers
 {
@@ -7,12 +9,37 @@ namespace SmartSchool_WebAPI.Controllers
     [Route("api/[controller]")]
     public class AlunoController : ControllerBase
     {
+        private readonly IRepository _repo;
+
+        public AlunoController(IRepository repo)
+        {
+            _repo = repo;
+        }
+
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
             try
             {
-                return Ok("");
+                var result = await _repo.GetAllAlunosAsync(true);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{AlunoId}")]
+        public async Task<IActionResult> GetByAlunoId(int AlunoId)
+        {
+            try
+            {
+                var result = await _repo.GetAlunoAsyncById(AlunoId, true);
+
+                return Ok(result);
             }
             catch (Exception ex)
             {
