173d7e915744e492d2e54150b38798c39e907f58
kelvin-feltrin <kelvin-798@hotmail.com>
2024-02-27 21:29:50 -0300

    Arquivo environment angular

--- SmartSchool/src/app/alunos/aluno.service.ts
+++ SmartSchool/src/app/alunos/aluno.service.ts
@@ -1,18 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlunoService {
 
-constructor(private http: HttpClient) { }
+  baseUrl = `${environment.UrlPrincipal}/api/aluno`;
+
+  constructor(private http: HttpClient) { }
 
   getAll() {
-    return this.http.get("");
+    return this.http.get(`${this.baseUrl}`);
   }
 
   getById(id: number) {
-    return this.http.get("url/${id}");
+    return this.http.get(`${this.baseUrl}/${id}`);
   }
 }
--- SmartSchool/src/environments/environment.prod.ts
+++ SmartSchool/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  UrlPrincipal: 'http://sejafullstack.com'
 };
