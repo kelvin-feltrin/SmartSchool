4e8cd4cf4244f2b207b79bd43e1e7b1fa582bf13
kelvin-feltrin <kelvin-798@hotmail.com>
2024-03-02 18:17:31 -0300

    Resolvendo problema NovoAluno

--- SmartSchool/src/app/models/Aluno.ts
+++ SmartSchool/src/app/models/Aluno.ts
@@ -1,4 +1,11 @@
 export class Aluno {
+    constructor() {
+        this.id = 0;
+        this.nome = '';
+        this.sobrenome = '';
+        this.telefone = 0;
+    }
+
     id: number = {} as number;
     nome: string = {} as string;
     sobrenome: string = {} as string;
